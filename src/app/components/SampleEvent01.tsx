c4e706eb37de0f33cbf1b7ef2647a81257edf4b0
Laksh Bharani <80910691+LakshBharani@users.noreply.github.com>
2025-04-30 16:00:08 -0400

    Update SampleEvent01.tsx

--- src/app/components/SampleEvent01.tsx
+++ src/app/components/SampleEvent01.tsx
@@ -1,3 +1,16 @@
+// import Image from "next/image";
+// import React from "react";
+// import logo from "../../assets/vt-event-01.png";
+
+// export default function SampleEvent01() {
+//   console.log("SampleEvent01");
+//   return (
+//     <div>
+//       <Image src={logo} alt="Event-01" className="h-screen w-screen" />
+//     </div>
+//   );
+// }
+
 import Image from "next/image";
 import React from "react";
 import logo from "../../assets/vt-event-01.png";
@@ -5,8 +18,8 @@ import logo from "../../assets/vt-event-01.png";
 export default function SampleEvent01() {
   console.log("SampleEvent01");
   return (
-    <div>
-      <Image src={logo} alt="Event-01" className="h-screen w-screen" />
+    <div className="h-screen w-screen bg-white flex justify-center items-center">
+      <Image src={logo} alt="Event-01" className="h-full w-full" />
     </div>
   );
 }
